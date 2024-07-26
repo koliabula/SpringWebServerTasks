73c6f23ff7342bfeeab4fd8fdd66c3452b799a8d
Bulla Kolia <koliakolan@gmail.com>
2024-07-26 12:51:25 +0300

    _

---
+++ src/main/java/bulla/kolia/springservertasks/SpringServerTasksApplication.java
@@ -0,0 +1,13 @@
+package bulla.kolia.springservertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bulla/kolia/springservertasks/SpringServerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package bulla.kolia.springservertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
